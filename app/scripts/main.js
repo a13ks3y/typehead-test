2dfd61eacecb4b12b2d3c444d3654c6a0353996e
a13ks3y <a13ks3y.ass@gmail.com>
2015-12-16 20:22:12 +0200

    display sorted states, add button to toggle sorting order

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -9,10 +9,13 @@ window.TypeheadTest = {
     init: function () {
         'use strict';
         this.states = new this.Collections.States();
+
         this.states.fetch().done(function (result) {
             this.searchField = new Backbone.Typeahead(result);
             this.searchField.setElement('#searchField').render();
-        });
+            this.stateList = new this.Views.StateList({ collection: this.states});
+            this.stateList.setElement('#stateList').render();
+        }.bind(this));
     }
 };
 
---
+++ app/scripts/views/state-list.js
@@ -0,0 +1,33 @@
+/*global TypeheadTest, Backbone, JST*/
+
+TypeheadTest.Views = TypeheadTest.Views || {};
+
+(function () {
+    'use strict';
+
+    TypeheadTest.Views.StateList = Backbone.View.extend({
+
+        template: JST['app/scripts/templates/state-list.ejs'],
+
+        isASC: true,
+
+        events: {
+            "click #toggleSort": "toggleSort"
+        },
+
+        toggleSort : function () {
+            this.isASC = !this.isASC;
+            this.render();
+        },
+
+        render: function () {
+            var states = this.collection.sortBy('name');
+
+            this.$el.html(this.template({
+                states: !this.isASC ? states : states.reverse()
+            }));
+        }
+
+    });
+
+})();
